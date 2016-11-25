e4fd7f91d5af61498d75005bf4c5f19909f18097
Tech-A39 <Tech-A39@rich_0121m.uk.brad-ex.co.uk>
2016-11-25 16:20:18 +0000

    Added resposive scrolling, setting up for skills progress bar

--- js/update-nav.js
+++ js/update-nav.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
-    $(window).scroll(function(){
-        if($(window).scrollTop() > $(window).height()){
-            $(".navbar-nav ul li a").css({"background-color":"transparent"});   
-        }
-        else{
-            $(".navbar-nav ul li a").css({"background-color":"white"});
-        }
+    // $(window).scroll(function(){
+    //     if($(window).scrollTop() > $(window).height()){
+    //         $(".navbar-nav ul li a").css({"background-color":"transparent"});   
+    //     }
+    //     else{
+    //         $(".navbar-nav ul li a").css({"background-color":"white"});
+    //     }
 
-    })
+    // })
+
+    $("#educationLink").click(function (){
+        var offset = 20; 
+            $('html, body').animate({
+                scrollTop: $("#education").offset().top + offset
+            }, 500);
+    });
+
 });
