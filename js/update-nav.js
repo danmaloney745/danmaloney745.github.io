f2cd6e0cefcaa51b94de0320bde0c5943a30d34b
Tech-A39 <Tech-A39@rich-0110.uk.brad-ex.co.uk>
2016-11-29 15:31:41 +0000

    Working image modal w/t carousel

--- js/update-nav.js
+++ js/update-nav.js
@@ -1,10 +1,37 @@
 $(document).ready(function(){
-    $('.thumbnail').click(function(){
-        $('.modal-body').empty();
-  	    var title = $(this).parent('a').attr("title");
-  	    $('.modal-title').html(title);
-  	    $($(this).parents('div').html()).appendTo('.modal-body');
-  	    $('#myModal').modal({show:true});
+    /* copy loaded thumbnails into carousel */
+    $('.row .thumbnail').on('load', function() {
+    
+    }).each(function(i) {
+        if(this.complete) {
+            var item = $('<div class="item"></div>');
+            var itemDiv = $(this).parents('div');
+            var title = $(this).parent('a').attr("title");
+            
+            item.attr("title",title);
+            $(itemDiv.html()).appendTo(item);
+            item.appendTo('.carousel-inner'); 
+            if (i==0){ // set first item active
+            item.addClass('active');
+            }
+        }
+    });
+
+    /* activate the carousel */
+    $('#modalCarousel').carousel({interval:false});
+
+    /* change modal title when slide changes */
+    $('#modalCarousel').on('slid.bs.carousel', function () {
+        $('.modal-title').html($(this).find('.active').attr("title"));
+    })
+
+    /* when clicking a thumbnail */
+    $('.row .thumbnail').click(function(){
+        var idx = $(this).parents('div').index();
+        var id = parseInt(idx);
+        $('#myModal').modal('show'); // show the modal
+        $('#modalCarousel').carousel(id); // slide carousel to selected
+        
     });
 
     /*Navbar Navigation Functions */
@@ -39,18 +66,5 @@ $(document).ready(function(){
     $('.progress .progress-bar').css("width", function() {
         return $(this).attr("aria-valuenow") + "%";
     })
-
-    var currentModal = $(this);
-    //click next
-    currentModal.find('.btn-next').click(function(){
-        currentModal.modal('hide');
-        currentModal.closest("div[id^='myModal']").nextAll("div[id^='myModal']").first().modal('show'); 
-    });
-  
-    //click prev
-    currentModal.find('.btn-prev').click(function(){
-        currentModal.modal('hide');
-        currentModal.closest("div[id^='myModal']").prevAll("div[id^='myModal']").first().modal('show'); 
-     });
 
 });
