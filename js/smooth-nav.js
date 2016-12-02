a64f9e6f054adcac4df9d2c7d40c6864aa13b796
Tech-A39 <Tech-A39@rich-0110.uk.brad-ex.co.uk>
2016-12-02 12:00:12 +0000

    Styling

--- js/smooth-nav.js
+++ js/smooth-nav.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){ 
-    //jQuery to collapse the navbar on scroll
-    // $(window).scroll(function() {
-    //     if ($(".navbar").offset().top > 50) {
-    //         $(".navbar-fixed-top").addClass("top-nav-collapse");
-    //     } else {
-    //         $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    //     }
-    // });
+
+    // Initialize affix and add an offset to add affix class on scroll
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
     $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
         var target = $(this.hash);
@@ -14,43 +14,9 @@ $(document).ready(function(){
         if (target.length) {
             $('html, body').animate({
             scrollTop: target.offset().top
-            }, 1000);
+            }, 1500);
             return false;
         }
         }
     });
-    
-    /*Navbar Navigation Functions */
-    /*
-    $("#educationLink").click(function (){
-        var offset = 20; 
-            $('html, body').animate({
-                scrollTop: $("#CV").offset().top + offset
-            }, 500);
-    });
-
-    $("#projectsLink").click(function (){
-        var offset = 20; 
-            $('html, body').animate({
-                scrollTop: $("#projects").offset().top + offset
-            }, 500);
-    });
-
-    $("#expeienceLink").click(function (){
-        var offset = 20; 
-            $('html, body').animate({
-                scrollTop: $("#experience").offset().top + offset
-            }, 500);
-    });
-
-    $("#contactDetailsLink").click(function (){
-        var offset = 20; 
-            $('html, body').animate({
-                scrollTop: $("#contact-details").offset().top + offset
-            }, 500);
-    });
-
-    $('.progress .progress-bar').css("width", function() {
-        return $(this).attr("aria-valuenow") + "%";
-    })*/
 });
